b35f8c3efed2b6465cafcca06832a5015541bac2
Martin <martin@clareandme.com>
2022-11-19 17:09:22 +0100

    add app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Paper, ThemeProvider, SxProps, Theme } from '@mui/material'
+import { Container } from '@mui/system'
+import { FC } from 'react'
+import BingoGrid from './components/BingoGrid'
+import BingoSizeSelect from './components/BingoSizeSelect'
+import ModeToggler from './components/ModeToggler'
+import GameState from './context/GameState'
+import CssBaseline from '@mui/material/CssBaseline'
+import { useDarkMode } from './hooks/useDarkMode'
+import { useWinAlert } from './hooks/useWinAlert'
+import BingoAlert from './components/BingoAlert'
+import BingoScreen from './components/BingoScreen'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const panelStyle: SxProps<Theme> = {
+  display: 'flex',
+  justifyContent: {
+    xs: 'space-around',
+  },
+  mb: 4,
+  alignItems: 'center',
+  flexDirection: {
+    xs: 'column-reverse',
+    sm: 'row',
+  },
 }
 
-export default App;
+const paperStyle: SxProps<Theme> = {
+  p: 4,
+  maxWidth: 'md',
+  m: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  backgroundColor: 'light',
+}
+
+const App: FC = () => {
+  const { mode, theme, toggleColorMode } = useDarkMode()
+  const { winAlert, setWinAlert, handleClose } = useWinAlert()
+
+  paperStyle.backgroundColor = mode === 'light' ? 'rgba(255,253,231,0.5)' : ''
+
+  return (
+    <GameState>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container sx={{ p: 4, backgroundColor: theme.palette.background.paper }}>
+          <BingoAlert winAlert={winAlert} handleClose={handleClose} />
+          <Paper sx={paperStyle} elevation={12}>
+            <Box sx={panelStyle}>
+              <BingoSizeSelect />
+              <BingoScreen />
+              <ModeToggler mode={mode} toggleColorMode={toggleColorMode} />
+            </Box>
+            <BingoGrid setWinAlert={setWinAlert} />
+          </Paper>
+        </Container>
+      </ThemeProvider>
+    </GameState>
+  )
+}
+
+export default App
